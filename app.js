95480e05ae1107b34c93748feb360cb86f7e1cee
Andrea Castro <andreacastro@adaptativamente.cl>
2022-07-31 18:26:11 -0400

    Ejemplo suma

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('Hola mundo');
+
+let numone = document.getElementById('numberone');
+let numtwo = document.getElementById('numbertwo');
+
+const sumar = () => {
+  console.log(Number(numone.value) + Number(numtwo.value));
+}
